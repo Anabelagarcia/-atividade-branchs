dd3c59dc9a5686666b00e13e7f6159b498d2e3b7
Anabelagarcia <anabelanams@gmail.com>
2024-10-31 14:15:37 -0400

    funcionalidade 3.0

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnCor').addEventListener('click', function() {
+    const divResultado = document.getElementById('resultado');
+    divResultado.style.color = divResultado.style.color === 'red' ? 'black' : 'red';
+});
